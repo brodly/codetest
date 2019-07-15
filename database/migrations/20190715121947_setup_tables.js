a2c6b3518e7cdd8962b2a6beb0ef674085cca5b6
Joshua Marx <marx.joshua@gmail.com>
2019-07-15 13:29:15 -0700

    name unique in users and cards. user_cards not nullable.

--- database/migrations/20190715121947_setup_tables.js
+++ database/migrations/20190715121947_setup_tables.js
@@ -1,11 +1,13 @@
 exports.up = knex => Promise.all([
   knex.schema.createTable('users', (table) => {
     table.increments();
+    table.unique('name');
     table.string('name').notNullable();
   }),
 
   knex.schema.createTable('cards', (table) => {
     table.increments();
+    table.unique('name');
     table.string('name').notNullable();
     table.string('desc').notNullable();
     table.string('fact').notNullable();
@@ -13,13 +15,13 @@ exports.up = knex => Promise.all([
   }),
 
   knex.schema.createTable('users_cards', (table) => {
-    table.integer('user_id');
-    table.integer('card_id');
+    table.integer('user_id').notNullable();
+    table.integer('card_id').notNullable();
   }),
 ]);
 
 exports.down = knex => Promise.all([
   knex.schema.dropTable('users'),
   knex.schema.dropTable('cards'),
-  knex.schema.dropTable('user_card'),
+  knex.schema.dropTable('users_cards'),
 ]);
