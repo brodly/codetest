44bc05ec387ed39b10f29d2239653b90e9822b65
Joshua Marx <marx.joshua@gmail.com>
2019-07-15 13:01:56 -0700

    created knex config for development

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const connection = require('./config');
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection,
+    migrations: {
+      directory: path.join(__dirname, '/database/migrations'),
+    },
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
