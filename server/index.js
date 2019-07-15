d5fb3169eaaad40e4054f84e6593c3742ab410b6
Joshua Marx <marx.joshua@gmail.com>
2019-07-15 16:17:10 -0700

    Implemented API routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes');
 
 const app = express();
 const port = 3000;
-
 const staticDir = path.join(__dirname, '..', '/public');
 
 app.use(express.static(staticDir));
+app.use('/api', api);
 
 app.listen(port, () => { console.log(`Listening on port: ${port}`)});
