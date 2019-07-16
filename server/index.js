de76b85d8454f9feed268c55f6513d5f390e594f
Joshua Marx <marx.joshua@gmail.com>
2019-07-15 21:26:01 -0700

    added process.env.PORT

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const path = require('path');
 const api = require('./routes');
 
 const app = express();
-const port = 3000;
-const staticDir = path.join(__dirname, '..', '/public');
+const port = process.env.PORT || 3000;
 
-app.use(express.static(staticDir));
+app.use(express.static(path.join(__dirname, '..', '/public')));
 app.use('/api', api);
 
-app.listen(port, () => { console.log(`Listening on port: ${port}`)});
+app.listen(port, () => { console.log(`Listening on port: ${port}`); });
