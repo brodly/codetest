67e1ae6c9c9d866c493d149b9d52b515de8abcba
Joshua Marx <marx.joshua@gmail.com>
2019-07-16 16:50:42 -0700

    removed id before updating entry into db

--- database/controller/index.js
+++ database/controller/index.js
@@ -6,6 +6,9 @@ module.exports = {
   createCard: ({ name, desc, fact, img_url }) => db.raw('insert into cards values (default, ?, ?, ?, ?);', [name, desc, fact, img_url]),
   getCard: name => db('cards').where(name),
   getAllCards: () => db.select().table('cards'),
-  updateCard: (name, data) => { db('cards').where({ name }).update(data); },
+  updateCard: (name, data) => {
+    delete data.id;
+    return db('cards').where('name', name).update(data);
+  },
   deleteCard: (name) => { db('cards').where({ name }).del(); },
 };
