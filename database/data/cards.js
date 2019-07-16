433a49d8df08cd42a39fdb786f1faaa4aa808877
Joshua Marx <marx.joshua@gmail.com>
2019-07-16 10:01:52 -0700

    First pass at designing working data

---
+++ database/data/cards.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Bitcoin',
+    desc: 'A Peer-to-Peer Electronic Cash System',
+    fact: 'Named in homage to bitcoins creator, a satoshi is the smallest amount within bitcoin representing 0.00000001 bitcoins',
+    img_url: 'https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png',
+  },
+  {
+    name: 'Ethereum',
+    desc: 'A blockchain-based distributed computing platform and operating system',
+    fact: 'Ethereums block time is 14 to 15 seconds, compared with 10 minutes for bitcoin.',
+    img_url: 'https://dynamic-assets.coinbase.com/7796fb1bd5752a156c77e0b1fa28fb17e93d03b4b8e2dcff58dcaf8f99b2e4a3c3af2cd905d5536761c54ac273d62d91a38ef8e5702fa988c37264e7aba36b3b/asset_icons/3af4b33bde3012fd29dd1366b0ad737660f24acc91750ee30a034a0679256d0b.png',
+  },
+];
