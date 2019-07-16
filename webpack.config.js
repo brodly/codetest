7586543aa5624ebd7dcb115afad5b2eba97c14ab
Joshua Marx <marx.joshua@gmail.com>
2019-07-16 10:02:46 -0700

    Implemented regenerator-runtime plugin in entry path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname + '/src/index.jsx'),
+  entry: ['regenerator-runtime/runtime', path.join(__dirname, '/src/index.jsx')],
   module: {
     rules: [
       {
