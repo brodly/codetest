75aa2a1be414bd1ec75f1a5b2e53e3eee7f9c8b3
Joshua Marx <marx.joshua@gmail.com>
2019-07-17 12:52:16 -0700

    Created DisplayCard, EditCard, Modal, NewCard components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,6 @@
-export { default as Card } from './Card';
 export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as EditCard } from './EditCard';
+export { default as NewCard } from './NewCard';
+export { default as DisplayCard } from './DisplayCard';
+export { default as Modal } from './Modal';
