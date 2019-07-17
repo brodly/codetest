e98ed4cc7acba4176d197c0647e34e1190f5a151
Joshua Marx <marx.joshua@gmail.com>
2019-07-16 17:05:00 -0700

    Added update card route. Cleaned up for readability

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,9 @@
 const axios = require('axios');
 
 module.exports = {
-  fetchCard: name => axios.get(`/api/card/${name}`)
-    .then(res => res.data),
-
-  fetchCards: () => axios.get('/api/card')
-    .then(res => res.data),
-
+  fetchCard: name => axios.get(`/api/card/${name}`).then(res => res.data),
+  fetchCards: () => axios.get('/api/card').then(res => res.data),
   createCard: card => axios.post('/api/card', card),
-
   deleteCard: name => axios.delete(`/api/card/${name}`),
+  updateCard: (name, data) => axios.patch(`/api/card/${name}`, data),
 };
